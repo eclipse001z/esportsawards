21797ae96ee7b201e1c27e9040771b4875b43378
eclipse <166458255+eclipse001z@users.noreply.github.com>
2024-06-28 04:13:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,9 @@ if __name__ == "__main__":
     elif is_int(num_requests):
         num_requests = int(num_requests)
     else:
-        print('Invalid input. Please enter a number or "infinite".')
-        sys.exit()
+        print('invalid input, defaulting to infinite) # not a number or "infinite", defaults to infinite
+        time.sleep(1)
+        num_requests = float('inf')
+        # sys.exit()
     
     asyncio.run(main(num_requests))
